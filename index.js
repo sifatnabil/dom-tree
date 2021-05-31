b6315d2fc18e7004c16a15c96cae1e64d55d453c
sifatnabil <sifatnabil@gmail.com>
2021-05-31 18:14:14 +0600

    MiniMax Algorithm Implementation

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 
-// const url =
-//   "https://web.ics.purdue.edu/~gchopra/class/public/pages/webdesign/05_simple.html";
+const url =
+  "https://web.ics.purdue.edu/~gchopra/class/public/pages/webdesign/05_simple.html";
 
-const url = "https://pubmed.ncbi.nlm.nih.gov/28683860/";
+// const url = "https://pubmed.ncbi.nlm.nih.gov/28683860/";
+const treeAr = [];
+
+const getLinearAr = (node) => {
+  if (node.children.length > 0) {
+    for (let j = 0; j < node.children.length; j++) {
+      getLinearAr(node.children[j]);
+    }
+  }
+
+  treeAr.push({
+    nodeName: node.nodeName,
+    parentName: node.parentName,
+    children: node.children.length,
+    content: node.content,
+    nodeVal: node.nodeVal,
+    nodeScore: node.nodeScore,
+  });
+};
+
+const optimizedValue = (node, isMax) => {
+  if (node.children.length == 0) {
+    return node.nodeScore;
+  }
+
+  let scores = [];
+  if (isMax) {
+    for (let j = 0; j < node.children.length; j++) {
+      scores.push(optimizedValue(node.children[j]), false);
+    }
+
+    return Math.max(...scores);
+  } else {
+    for (let j = 0; j < node.children.length; j++) {
+      scores.push(optimizedValue(node.children[j]), true);
+    }
+
+    return Math.min(...scores);
+  }
+};
 
 const getTree = async () => {
   const browser = await puppeteer.launch({ headless: true });
@@ -16,13 +55,14 @@ const getTree = async () => {
     let nodeValue = 0;
     const getNodeTree = (node) => {
       if (node.hasChildNodes()) {
-        var children = [];
-        for (var j = 0; j < node.childNodes.length; j++) {
-          children.push(getNodeTree(node.childNodes[j]));
+        let children = [];
+        for (let j = 0; j < node.childNodes.length; j++) {
+          const child = getNodeTree(node.childNodes[j]);
+          if (child) children.push(child);
         }
 
         nodeValue++;
-        let score;
+        let score = 0;
         let content = node.outerText || "";
         if (content) {
           const contentWords = content.split(" ");
@@ -48,7 +88,29 @@ const getTree = async () => {
     return nodeTree;
   });
 
-  console.log(tree);
+  // console.log(tree);
+
+  // const optimedNodeContent = optimizedValue(tree, true);
+  // console.log(optimedNodeContent);
+
+  getLinearAr(tree);
+  // console.log(treeAr);
+
+  treeAr.sort((a, b) => {
+    return b.nodeScore - a.nodeScore;
+  });
+
+  console.log("Sorting based on node score Descending");
+
+  console.log(treeAr);
+
+  treeAr.sort((a, b) => {
+    return a.children - b.children;
+  });
+
+  console.log("Sorting based on no of children Ascending");
+
+  console.log(treeAr);
 
   await browser.close();
 };
